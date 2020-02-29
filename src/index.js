875bb15f793dc416a3167eccccb2ad5b60d92d6e
as-vladislav-k <vladislav.k.as@agiliumlabs.com>
2020-02-29 11:26:37 +0200

    Fix bug

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { useUploader } from './hooks/useUploader';
+import { useDownloader } from './hooks/useDownloader';
+import { DownloadService } from './download-service/download.service';
+
+export {
+  useDownloader,
+  useUploader,
+  DownloadService
+}
